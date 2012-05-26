5262faaabc8d1bc648a9e73660c16d3af4dc6369
Alan Zimmerman <alan.zimm@gmail.com>
2012-05-26 11:41:41 +0200

    Snapshot

--- test.hs
+++ test.hs
@@ -51,6 +51,19 @@ identifierGen = neStringOf identifierStart identifierPart
     identifierStart = ['a'..'z']++['A'..'Z'] ++ ['_']
     identifierPart = identifierStart ++ ['0'..'9']
 
+jsIdentifierGen :: Gen JSNode
+jsIdentifierGen = liftM  (JSIdentifier JSNoAnnot) identifierGen
+
+-- ---------------------------------------------------------------------
+
+nullLiteralGen :: Gen [Char]
+nullLiteralGen = elements ["null"]
+
+-- ---------------------------------------------------------------------
+
+booleanLiteralGen :: Gen [Char]
+booleanLiteralGen = elements ["true","false"]
+
 -- ---------------------------------------------------------------------
 {-
 DecimalLiteral ::
@@ -63,31 +76,83 @@ decimalGen :: Gen [Char]
 decimalGen = neStringOf ['0'..'9'] ['0'..'9']
 
 -- ---------------------------------------------------------------------
+{-
+HexIntegerLiteral ::
+  0x HexDigit
+  0X HexDigit
+  HexIntegerLiteral HexDigit
+-}
+hexIntegerGen :: Gen [Char]
+hexIntegerGen = do
+  s <- elements ["0x","0X"]
+  let hexDigits = ['0'..'9'] ++ ['a'..'f']++['A'..'F']
+  r <- neStringOf hexDigits hexDigits
+  return (s++r)
 
+-- ---------------------------------------------------------------------
+
+-- TODO: include escape chars etc.
 stringLiteralGen :: Gen [Char]
 stringLiteralGen = arbitrary
+
+-- ---------------------------------------------------------------------
+
+-- TODO: extend / fix
+regexLiteralGen :: Gen [Char]
+regexLiteralGen = do
+  body <- stringLiteralGen
+  flags <- listOf' $ elements (['a'..'z']++['A'..'Z'])
+  return (['/']++body++['/']++flags)
 
 -- ---------------------------------------------------------------------
 
 instance Arbitrary JSNode where
   arbitrary = oneof
               [
-                liftM (JSIdentifier JSNoAnnot) identifierGen
-              , liftM (JSDecimal JSNoAnnot) decimalGen
-              , liftM (JSLiteral JSNoAnnot) stringLiteralGen
+                -- Literals
+                liftM  (JSLiteral       JSNoAnnot) nullLiteralGen
+              , liftM  (JSLiteral       JSNoAnnot) booleanLiteralGen
+              , liftM  (JSDecimal       JSNoAnnot) decimalGen
+              , liftM  (JSHexInteger    JSNoAnnot) hexIntegerGen
+              , liftM2 (JSStringLiteral JSNoAnnot) (elements ['\'','"']) stringLiteralGen
+              , liftM  (JSRegEx         JSNoAnnot) regexLiteralGen
+
+
+              --, liftM  (JSIdentifier JSNoAnnot) identifierGen
+              , jsIdentifierGen
+              , liftM  (JSLiteral JSNoAnnot) stringLiteralGen
 
               ]
 
-{-
-instance Arbitrary JSBlock where
-  arbitrary = JSBlock JSAnnot [JSStatement] JSAnnot
 
 instance Arbitrary JSBlock where
-  arbitrary = undefined
+  arbitrary = liftM mkBlock (arbitrary :: Gen [JSStatement])
+    where
+      mkBlock xs = JSBlock JSNoAnnot xs JSNoAnnot
+
+
+emptyJSNode :: Gen [JSNode]
+emptyJSNode = elements [[]]
+
+optionalIdentifierGen :: Gen [JSNode]
+optionalIdentifierGen = do
+  oneof [(vectorOf 0 jsIdentifierGen),(vectorOf 1 jsIdentifierGen)]
+
+breakStatementGen :: Gen JSStatement
+breakStatementGen = liftM mkBreak optionalIdentifierGen
+  where
+    mkBreak xs = JSBreak JSNoAnnot xs JSSemiAuto
+
+blockStatementGen :: Gen JSStatement
+blockStatementGen = liftM JSStatementBlock arbitrary
 
 instance Arbitrary JSStatement where
-  arbitrary = undefined
--}
+  arbitrary = oneof
+              [
+                breakStatementGen
+              , blockStatementGen
+              ]
+
 --instance Arbitrary JSAST where
 --  arbitrary   = JSSourceElementsTop (elements [[],])
 
