379db5890b43ea15837b7e93de39aa1e4130584e
Erik de Castro Lopo <erikd@mega-nerd.com>
2015-08-10 20:04:44 +1000

    Remove file buildall.sh, fix cabal file

--- buildall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# do a clean build of all, including the tests
-#cabal clean && cabal configure -fbuildtests && cabal build && cabal haddock
-cabal clean && cabal configure --enable-tests && cabal build && cabal test && cabal haddock
