57d40d6e5871b22b77e469503d37b8e0af76a871
Erik de Castro Lopo <erikd@mega-nerd.com>
2016-03-19 11:35:57 +1100

    Text.Jasmine: Use `minifyJS` from language-javascript
    In the process, drop the fragile ad-hoc minification that was used
    previously.

--- Text/Jasmine.hs
+++ Text/Jasmine.hs
@@ -9,38 +9,39 @@ import Control.Applicative ((<$>))
 import Data.Text.Lazy (unpack)
 import Data.Text.Lazy.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
-import Language.JavaScript.Parser (readJs, parse, JSNode(..))
+import Language.JavaScript.Parser (readJs, parse, JSAST)
+import Language.JavaScript.Pretty.Printer
+import Language.JavaScript.Process.Minify
 
 import qualified Blaze.ByteString.Builder as BB
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString.Lazy.Char8 as S8
 
-import Text.Jasmine.Pretty
 
 
 minifym :: LB.ByteString -> Either String LB.ByteString
 minifym s =
     case myParse s of
         Left msg -> Left (show msg)
-        Right p  -> Right $ BB.toLazyByteString $ renderJS p
+        Right p  -> Right $ BB.toLazyByteString $ renderJS $ minifyJS p
 
 
 minifyBb :: LB.ByteString -> Either String BB.Builder
 minifyBb s =
     case myParse s  of
         Left msg -> Left (show msg)
-        Right p  -> Right (renderJS p)
+        Right p  -> Right (renderJS $ minifyJS p)
 
 
 minify :: LB.ByteString -> LB.ByteString
-minify s = BB.toLazyByteString . renderJS . readJs $ lbToStr s
+minify s = BB.toLazyByteString . renderJS . minifyJS . readJs $ lbToStr s
 
 
 minifyFile :: FilePath -> IO LB.ByteString
 minifyFile filename = minify <$> LB.readFile filename
 
 
-myParse :: S8.ByteString -> Either String JSNode
+myParse :: S8.ByteString -> Either String JSAST
 myParse input = parse (lbToStr input) "src"
 
 
