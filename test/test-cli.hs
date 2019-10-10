08d2e689c4b3bf31cbcddbe62b7cc53135a1b114
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-11 07:24:02 +1100

    test: Fix and improve test runner

--- test/test-cli.hs
+++ test/test-cli.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE MultiWayIf #-}
 
-import           Control.Monad (forM_)
-import           System.Directory (listDirectory)
-import           System.Process (callProcess)
+import           Control.Monad (foldM, forM)
+import           Control.Monad.Extra (concatMapM)
+
+import           System.Directory (canonicalizePath, getCurrentDirectory, listDirectory)
+import           System.Environment (setEnv)
+import           System.FilePath (takeFileName, (</>))
+import           System.Exit (ExitCode (..), exitFailure, exitSuccess)
+import           System.Posix.Files (getFileStatus, isDirectory, isRegularFile)
+import           System.Process (rawSystem)
 import           System.IO (BufferMode (..))
 import qualified System.IO as IO
 
@@ -9,9 +16,42 @@ main :: IO ()
 main = do
   IO.hSetBuffering IO.stdout LineBuffering
   IO.hSetBuffering IO.stderr LineBuffering
-  testCliMain ["./dist/build/hjsmin/hjsmin"]
 
-testCliMain :: [String] -> IO ()
-testCliMain args = do
+  cwd <- getCurrentDirectory
+  topdir <- canonicalizePath $ cwd </> "dist-newstyle"
+
+  -- Set an environment variable for all the exectuables we want to test.
+  setExecutableEnvVar "HJSMIN" topdir "hjsmin"
+
   tests <- filter (`notElem` ["core", "data"]) <$> listDirectory "test/cli/"
-  forM_ tests $ \ t -> callProcess ("test/cli/" ++ t ++ "/run") args
+  res <- forM tests $ \ t -> rawSystem ("test/cli/" ++ t ++ "/run") []
+  if all (== ExitSuccess) res
+    then exitSuccess
+    else exitFailure
+
+
+setExecutableEnvVar :: String -> FilePath -> FilePath -> IO ()
+setExecutableEnvVar envName startDir target = do
+  xs <- listDirectoryRecursive startDir
+  case filter match xs of
+    [] -> error "Unable to find hjsmin binary"
+    [x] -> setEnv envName x
+    _ -> error $ "Multiple binaries: " ++ show xs
+ where
+  match :: FilePath -> Bool
+  match fp = takeFileName fp == target
+
+listDirectoryRecursive :: FilePath -> IO [FilePath]
+listDirectoryRecursive fpath = do
+  xs <- fmap (fpath </>) <$> listDirectory fpath
+  (files, dirs) <- foldM partitioner ([], []) xs
+  rest <- concatMapM listDirectoryRecursive (dirs :: [FilePath])
+  pure $ files ++ rest
+ where
+  partitioner :: ([FilePath], [FilePath]) -> FilePath -> IO ([FilePath], [FilePath])
+  partitioner (files, dirs) fp = do
+    st <- getFileStatus fp
+    if
+      | isRegularFile st -> pure (fp : files, dirs)
+      | isDirectory st -> pure (files, fp : dirs)
+      | otherwise -> pure (files, dirs)
